7160eacff1652489f5c169a27e3e0e2ce09c4ac0
Fevi-lendu <150247854+Fevi-lendu@users.noreply.github.com>
2023-12-17 17:48:59 -0800

    js added

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -6,17 +6,52 @@ document.addEventListener('DOMContentLoaded', function() {
     window.location.href = 'about.html';
             });
         });
+        const linkedin = document.querySelector('.fa-linkedin');
 
-document.addEventListener('DOMContentLoaded', function() {
-var headerMenuBar = document.getElementById('header_menubar');
-var dropdown = document.getElementsByClassName('dropdown');
-        
-headerMenuBar.addEventListener('click', function() {
-    if (dropdown.style.display === 'none') {
-    console.log('klkl')
-              dropdown.style.display = 'block';
-            } else {
-              dropdown.style.display = 'none';
-            }
-          });
+  
+        linkedin.addEventListener('click', function () {
+            window.open('https://www.linkedin.com/in/feven-lendu-41ab912a4/', '_blank');
+        });
+        const githubIcon = document.querySelector('.fa-github');
+
+        githubIcon.addEventListener('click', function () {
+            window.open('https://github.com/Fevi-lendu', '_blank');
+        });
+        const telegram = document.querySelector('.fa-telegram');
+
+        telegram.addEventListener('click', function () {
+      
+            window.open('https://t.me/eftii', '_blank');
+        });
+        const twitter = document.querySelector('.fa-twitter');
+
+        twitter.addEventListener('click', function () {
+            
+            window.open('https://twitter.com', '_blank');
+        });
+        const homeSitemap = document.querySelector('.homeSitemap');
+        const aboutSitemap = document.querySelector('.aboutSitemap');
+        const testimonialSitemap = document.querySelector('.testimonialSitemap');
+        const educationSitemap = document.querySelector('.educationSitemap');
+        const contactSitemap = document.querySelector('.contactSitemap');
+
+   
+        homeSitemap.addEventListener('click', function () {
+            window.location.href = 'home.html';
+        });
+
+        aboutSitemap.addEventListener('click', function () {
+            window.location.href = 'about.html';
+        });
+
+        testimonialSitemap.addEventListener('click', function () {
+            window.location.href = 'testimonial.html';
+        });
+
+        educationSitemap.addEventListener('click', function () {
+            window.location.href = 'education.html';
+        });
+
+        contactSitemap.addEventListener('click', function () {
+            window.location.href = 'contact.html';
         });
